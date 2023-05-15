09bcb781ceda2072a42e9710694309bf73b8b271
Nirali Mamaniya <niralimamaniya29@gmail.com>
2023-05-15 06:42:40 +0530

    Igniting the app from scratch

--- App.js
+++ App.js
@@ -1,26 +1,5 @@
-// Creating a single element
-// const heading = React.createElement(
-//     "h2",
-//     {id:"heading"},
-//     "Hello World from React :)"
-// );
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-
-//  root.render(heading);
-
-
-// Creating nested elements / structure 
-/* <div id="parent">
-    <div id="child">
-        <h1></h1>
-        <h2></h2>
-    </div>
-    <div id="child2">
-        <h1></h1>
-        <h2></h2>
-    </div>
-</div> */ 
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = 
     React.createElement("div",{id:"parent"},[
         React.createElement("div",{id:"child"},[
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Hello React !
+# ReactJS !
+
+# Parcel
+
+- Dev Build
+- Local Server
+- HMR - Hot Module Replacement
+- File Watching Algortihm - written in c++
+- Caching - Faster Builds
+- Image optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling
+- Error Handling
+- Diagnostics
+- Tree shaking - remove unused code for you
+- Different development and production build
