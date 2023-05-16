1747f3e4bd3c5fd9f122afc21e90946d4b39d0a1
Nirali Mamaniya <niralimamaniya29@gmail.com>
2023-05-16 06:51:26 +0530

    Laying the foundation

--- App.js
+++ App.js
@@ -1,25 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = () => (
-    <h1 className="head">
-        Namaste React from JSX
-    </h1>
-);
+// Creating a nested header element without jsx
+// const headerWithoutJSX = React.createElement("div",{class:"title"},[
+//     React.createElement("h1",{},"This first is h1 tag"),
+//     React.createElement("h1",{},"This is second h1 tag"),
+//     React.createElement("h1",{},"This is third h1 tag")
+// ])
 
-const HeadingComponent = () => (
-    
-    <React.Fragment>
-        <div id="container">
-            {Title()}
-            <h1 className="head">
-                Namaste React from Component
-            </h1>   
-        </div>
-        <div></div>
-    </React.Fragment>
-    
-);
+// Creating header element using JSX
+// const headerWithJSX = (
+//     <div className="title">
+//         <h1>This is first h1 tag</h1>
+//         <h1>This is second h1 tag</h1>
+//         <h1>This is third h1 tag</h1>
+//     </div>
+// );
+
+// Creating a functional component with JSX
+// const HeaderComponent = () => (
+//     <div className="title">
+//         <h1>This is first h1 tag</h1>
+//         <h1>This is second h1 tag</h1>
+//         <h1>This is third h1 tag</h1>
+//     </div>
+// );
+
+const HeaderComponent = () => (
+    <div className="title">
+        <i className="bi bi-bell-fill bell"></i>
+        <input type="text" name="search" className="search-bar"></input>
+        <i class="bi bi-person-circle user"></i>
+    </div>
+)
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent/>);
+root.render(<HeaderComponent/>);
