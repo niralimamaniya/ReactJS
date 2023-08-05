5b7d3d1af7c5cf7d5ab589adad378982635be912
Nirali Mamaniya <niralimamaniya29@gmail.com>
2023-08-05 23:01:02 +0530

    Added CORS proxy

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,4 +3,4 @@ export const CDN_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,
 // export const CAROUSEL_CDN_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_520,h_520/rng/md/carousel/production/";
 export const CAROUSEL_CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_850,h_504/";
 
-export const MENU_API_URL =  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5177559&lng=73.81511119999999&restaurantId=";
+export const MENU_API_URL =  "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5177559&lng=73.81511119999999&restaurantId=";
